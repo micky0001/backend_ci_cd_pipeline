d6dc8cc8f9d707bb8b17b48703d6f73e1b81d67d
micky0001 <princeawan092@gmail.com>
2025-07-30 12:12:42 +0500

    staging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0'; // 🔁 Accept connections from anywhere
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World i am doing some changes to this file by using ci /cd pipelien');
+  res.send('Hello World i am doing some changes to this file by using ci /cd pipelien ok karo');
 });
 
 app.listen(PORT, HOST, () => {
